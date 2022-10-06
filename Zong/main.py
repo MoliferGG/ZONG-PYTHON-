1b48d61d9929f1fb818f132d438d030c9ada974c
Molifer <91330242+MoliferGG@users.noreply.github.com>
2022-10-06 16:18:33 +0530

    Update main.py

--- Zong/main.py
+++ Zong/main.py
@@ -277,7 +277,7 @@ running  = True
 while running: 
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
-            pygame.quit()
+            sys.exit()
             
     mainMenu()
     
